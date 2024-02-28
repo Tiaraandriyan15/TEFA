95e8700231d80c21f878bddd7a64e724410be01d
Tiaraandriyan15 <158125769+Tiaraandriyan15@users.noreply.github.com>
2024-02-28 15:46:30 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="row p-3 bg-primary text-white">
+        <div class="col-lg-1">
+            <img src='~/assets/img/LOGO.png' alt="logo SMK4" class="logo">
+        </div>
+        <div class="col-lg-11">
+            <h1>Perpus Digital</h1>
+            <address>
+            SMKN 4 TASIKMALAYA
+            Jl.Depok, Sukamenak, Purbaratu
+            </address>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.logo{
+    width: 100%;
+}
+</style>
---
+++ pages/pengunjung/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+                <div class="my-3">
+                    <input type="search" class="form-control from-control-lg rounded-5" placeholder="Filter...">
+                </div>
+                <div class="my-3 text-muted">menampilkan 1 dari 1</div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <td>#</td>
+                            <td>NAMA</td>
+                            <td>KATEGORI</td>
+                            <td>WAKTU</td>
+                            <td>KEPERLUAN</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>1.</td>
+                        <td>Tiara</td>
+                        <td>Siswa</td>
+                        <td>24 Februari, 23.31.00</td>
+                        <td>Baca</td>
+                    </tr>
+                </tbody>
+                </table>
+                <nuxt-link to="http://localhost:3000/">
+                    <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">kembali</button>
+                </nuxt-link>
+            </div>
+        </div>
+    </div>
+</template>
